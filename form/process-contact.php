36925168d0e9ad864810bbcc143fc94a7a8ec7a3
saziya ansari <saziyaaansari.30@gmail.com>
2026-02-16 20:01:08 +0530

    commit

---
+++ form/process-contact.php
@@ -0,0 +1,43 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recipient email
+    $to_email = "saziyaaansari.30@gmail.com"; 
+    
+    // Subject
+    $subject = "New Contact Form Submission from Dr Prafful Jatale Website";
+
+    // Sanitize and validate input
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $phone = strip_tags(trim($_POST["phone"]));
+    $message = strip_tags(trim($_POST["message"]));
+
+    // Check availability of required fields
+    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        echo "Please complete the form and try again.";
+        exit;
+    }
+
+    // Email Body
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Phone: $phone\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Email Headers
+    $email_headers = "From: $name <$email>";
+
+    // Send email
+    if (mail($to_email, $subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
